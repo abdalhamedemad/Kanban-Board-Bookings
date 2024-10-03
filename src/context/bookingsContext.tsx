952728b8db7b0c370b46cb26d203393c52417ed1
abdalhamedemad <76442606+abdalhamedemad@users.noreply.github.com>
2024-10-03 22:44:34 +0300

    feat: add booking context

---
+++ src/context/bookingsContext.tsx
@@ -0,0 +1,99 @@
+import { useState, createContext, useContext, useEffect } from "react";
+import useLocalStorage from "../Hooks/useLocalStorage.js";
+
+const BookingsContext = createContext({
+  unclaimedMembers: [],
+  firstContactMembers: [],
+  preparingWorkOffer: [],
+  sentToTherapists: [],
+});
+
+const BookingsContextProvider = (props) => {
+  const [members, setMembers] = useLocalStorage("members", null);
+
+  const [unclaimedMembers, setUnclaimedMembers] = useState(() => {
+    if (members !== null) {
+      return JSON.parse(members).filter(
+        (member) => member.status === "unclaimed"
+      );
+    }
+    return [];
+  });
+
+  const [firstContactMembers, setFirstContactMembers] = useState(() => {
+    if (members !== null) {
+      return JSON.parse(members).filter(
+        (member) => member.status === "firstContact"
+      );
+    }
+    return [];
+  });
+
+  const [preparingWorkOffer, setPreparingWorkOffer] = useState(() => {
+    if (members !== null) {
+      return JSON.parse(members).filter(
+        (member) => member.status === "preparingWorkOffer"
+      );
+    }
+    return [];
+  });
+
+  const [sentToTherapists, setSentToTherapists] = useState(() => {
+    if (members !== null) {
+      return JSON.parse(members).filter(
+        (member) => member.status === "sentToTherapists"
+      );
+    }
+    return [];
+  });
+  useEffect(() => {
+    if (members !== null) {
+      setUnclaimedMembers(
+        JSON.parse(members).filter((member) => member.status === "unclaimed")
+      );
+
+      setFirstContactMembers(
+        JSON.parse(members).filter((member) => member.status === "firstContact")
+      );
+
+      setPreparingWorkOffer(
+        JSON.parse(members).filter(
+          (member) => member.status === "preparingWorkOffer"
+        )
+      );
+
+      setSentToTherapists(
+        JSON.parse(members).filter(
+          (member) => member.status === "sentToTherapists"
+        )
+      );
+    }
+  }, [members]);
+
+  const addNewMember = (newMember) => {
+    if (members === null) setMembers(JSON.stringify([newMember]));
+    else {
+      console.log("Members already exist", [...JSON.parse(members), newMember]);
+      setMembers(JSON.stringify([...JSON.parse(members), newMember]));
+    }
+    console.log("unclaimedMembers", unclaimedMembers);
+  };
+
+  const value = {
+    unclaimedMembers,
+    firstContactMembers,
+    preparingWorkOffer,
+    sentToTherapists,
+    addNewBooking,
+  };
+
+  return (
+    <BookingsContext.Provider value={value}>
+      {props.children}
+    </BookingsContext.Provider>
+  );
+};
+const useBookings = () => {
+  return useContext(BookingsContext);
+};
+export { useBookings, BookingsContextProvider };
--- src/pages/CreateBooking.tsx
+++ src/pages/CreateBooking.tsx
@@ -1,10 +1,10 @@
-
 function CreateBooking() {
-  return ( 
-    <div >
-      <h1>Create Booking</h1>
-      </div>
-   );
+  return (
+    <div className="max-w-3xl mx-auto px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Add New Patient</h2>
+      form
+    </div>
+  );
 }
 
 export default CreateBooking;
