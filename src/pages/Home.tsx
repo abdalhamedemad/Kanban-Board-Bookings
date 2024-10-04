898b0364e46724266aa5cc6fb78a2042f27f676b
abdalhamedemad <76442606+abdalhamedemad@users.noreply.github.com>
2024-10-04 09:13:02 +0300

    feat: add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,27 @@
-import CreateBookingForm from "../components/CreateBookingForm";
-import Modal from "../ui/Modal";
-import { useState } from "react";
-function Home() {
-  const [isModalOpen, setModalOpen] = useState(false);
+import Button from "../ui/Button";
 
-  const openModal = () => setModalOpen(true);
-  const closeModal = () => setModalOpen(false);
+function Home() {
   return (
-    <div>
-      Home
-      <button
-        onClick={openModal}
-        className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-      >
-        Open Modal
-      </button>
-      <Modal isOpen={true} onClose={() => {}} title="Delete Booking">
-        <p>Are you sure you want to delete this booking?</p>
-        <div className="flex gap-2 mt-5">
-          <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-            Delete
-          </button>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Close
-          </button>
+    <div className="max-w-3xl mx-auto px-4 py-6 text-[#476c98] mt-5 font-mono">
+      <div className="flex flex-col items-center justify-center gap-16">
+        <h1
+          className="
+      text-4xl font-semibold text-center
+      "
+        >
+          Welcome to Kanban Board Booking
+        </h1>
+        <div>
+          <div className="flex items-center justify-start gap-4">
+            <Button type="primary" to="/create-booking">
+              Create Booking
+            </Button>
+            <Button type="primary" to="/board-booking">
+              View Bookings
+            </Button>
+          </div>
         </div>
-      </Modal>
+      </div>
     </div>
   );
 }
